8a29ede2d2cf0fa8026ba4c7715040329f68eb14
Fabian Vilers <fabian.vilers@dev-one.com>
2019-02-18 09:26:19 +0100

    Export a function to disable the react dev tools

---
+++ index.js
@@ -0,0 +1,19 @@
+import { isFunction, isObject } from 'lodash';
+
+export function disableReactDevTools() {
+  // Ensure the React Developer Tools global hook exists
+  if (!isObject(window.__REACT_DEVTOOLS_GLOBAL_HOOK__)) {
+    return;
+  }
+
+  const NO_OP = () => {};
+
+  // Replace all global hook properties with a no-op function or a null value
+  for (const prop in window.__REACT_DEVTOOLS_GLOBAL_HOOK__) {
+    window.__REACT_DEVTOOLS_GLOBAL_HOOK__[prop] = isFunction(
+      window.__REACT_DEVTOOLS_GLOBAL_HOOK__[prop]
+    )
+      ? NO_OP
+      : null;
+  }
+}
