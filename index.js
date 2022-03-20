9371a0e07b55b1636034ecdc0f2389d559de2a5a
bybruno <oi@bybruno.dev>
2022-03-20 19:13:02 -0300

    adds support for nextjs apps

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-import { isFunction, isObject } from './helpers';
+import { isFunction, isObject, hasWindowObject } from './helpers';
 
 export function disableReactDevTools() {
-  // Ensure the React Developer Tools global hook exists
-  if (!isObject(window.__REACT_DEVTOOLS_GLOBAL_HOOK__)) {
-    return;
-  }
-
-  // Replace all global hook properties with a no-op function or a null value
-  for (const prop in window.__REACT_DEVTOOLS_GLOBAL_HOOK__) {
-    if (prop === "renderers") {
-      // prevents console error when dev tools try to iterate of renderers
-      window.__REACT_DEVTOOLS_GLOBAL_HOOK__[prop] = new Map();
-      continue;
+  if (hasWindowObject()) {
+    // Ensure the React Developer Tools global hook exists
+    if (!isObject(window.__REACT_DEVTOOLS_GLOBAL_HOOK__)) {
+      return;
     }
-    window.__REACT_DEVTOOLS_GLOBAL_HOOK__[prop] = isFunction(
-      window.__REACT_DEVTOOLS_GLOBAL_HOOK__[prop]
-    )
-      ? Function.prototype
-      : null;
+
+    // Replace all global hook properties with a no-op function or a null value
+    for (const prop in window.__REACT_DEVTOOLS_GLOBAL_HOOK__) {
+      if (prop === 'renderers') {
+        // prevents console error when dev tools try to iterate of renderers
+        window.__REACT_DEVTOOLS_GLOBAL_HOOK__[prop] = new Map();
+        continue;
+      }
+      window.__REACT_DEVTOOLS_GLOBAL_HOOK__[prop] = isFunction(
+        window.__REACT_DEVTOOLS_GLOBAL_HOOK__[prop]
+      )
+        ? Function.prototype
+        : null;
+    }
   }
 }
